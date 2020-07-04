6144b15ce80a95f1a7fa68450fdf06b842dfb3fd
AndrewZaza601 <59883771+AndrewZaza601@users.noreply.github.com>
2020-07-04 14:07:06 +0200

    lec9

--- Job Offers Website/Models/AccountViewModels.cs
+++ Job Offers Website/Models/AccountViewModels.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
@@ -64,19 +65,23 @@ namespace WebApplication1.Models
 
     public class RegisterViewModel
     {
+
+        [Required]
+        [DisplayName("أسم المستخدم")]
+        public string UserName { get; set; }
         [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
+        [Display(Name = "البريد الالكترونى")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "كلمة السر ")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "تاكيد كلمة السر")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }

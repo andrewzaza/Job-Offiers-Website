98101504bd17bbc7aa0b5ad1dc07924d1093e5e4
AndrewZaza601 <59883771+AndrewZaza601@users.noreply.github.com>
2020-07-09 01:28:38 +0200

    le22

--- Job Offers Website/Startup.cs
+++ Job Offers Website/Startup.cs
@@ -1,14 +1,38 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using WebApplication1.Models;
 
 [assembly: OwinStartupAttribute(typeof(WebApplication1.Startup))]
 namespace WebApplication1
 {
     public partial class Startup
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateDefaultRolesAndUsers();
+        }
+        public void CreateDefaultRolesAndUsers()
+        {
+            var roleManger = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var userManger = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            IdentityRole role = new IdentityRole();
+            if (!roleManger.RoleExists("Admins"))
+            {
+                role.Name = "Admins";
+                roleManger.Create(role);
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "Andrew";
+                user.Email = "andrewzaza915@gmail.com";
+                var Check = userManger.Create(user, "As224466$$");
+                if (Check.Succeeded)
+                {
+                    userManger.AddToRole(user.Id, "Admins");
+                }
+            }
         }
     }
 }

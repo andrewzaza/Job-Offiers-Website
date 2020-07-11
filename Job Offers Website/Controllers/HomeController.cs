f14956f11867e3edeb31df488cc315f8a6b958de
AndrewZaza601 <59883771+AndrewZaza601@users.noreply.github.com>
2020-07-11 17:39:12 +0200

    lec32

--- Job Offers Website/Controllers/HomeController.cs
+++ Job Offers Website/Controllers/HomeController.cs
@@ -86,6 +86,18 @@ namespace WebApplication1.Controllers
             }
             return View(job);
         }
+        [Authorize]
+        public ActionResult GetJobsByPublisher()
+        {
+            var UserID = User.Identity.GetUserId();
+            var Jobs = from app in db.ApplyForJobs
+                       join job in db.Jobs
+                       on app.JobId equals job.Id
+                       where job.User.Id == UserID
+                       select app;
+            return View(Jobs.ToList());
+        }
+       
         public ActionResult Edit(int id)
         {
             var job = db.ApplyForJobs.Find(id);
@@ -97,6 +109,8 @@ namespace WebApplication1.Controllers
             }
             return View(job);
         }
+
+
         [HttpPost]
         public ActionResult Edit(ApplyForJob job)
         {
